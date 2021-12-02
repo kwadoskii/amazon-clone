0b2fcf5be1ded750c8841952b7aa4849333644fb
Austin Ofor <kwadoskii@live.com>
2021-12-02 12:51:01 +0100

    exchange rate fix

--- components/Product.jsx
+++ components/Product.jsx
@@ -32,6 +32,8 @@ export default function Product({
     dispatch(addToBasket(product));
   };
 
+  const exchangeRate = 567; //exchange rate to Naira from USD
+
   return (
     <div className="relative flex flex-col m-4 bg-white z-30 p-10 rounded-md shadow-sm">
       <p className="absolute top-2 right-0 text-xs italic text-gray-700 bg-yellow-400 py-1 px-2 rounded-md rounded-r-none shadow-md ">
@@ -53,7 +55,7 @@ export default function Product({
       <p className="text-xs my-2 line-clamp-2">{description}</p>
 
       <div className="mb-5">
-        <Currency quantity={price * 567} currency="NGN" />
+        <Currency quantity={price * exchangeRate} currency="NGN" />
       </div>
 
       {rating > 4 && (
